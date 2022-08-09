9a199bd89ff4155ac26dd019e221d9aac811ea93
chenchuke <chuke_chen@163.com>
2022-08-09 09:50:52 +0800

    test relsolver

--- src/apollo/resolvers.ts
+++ src/apollo/resolvers.ts
@@ -2,10 +2,10 @@ import { PrismaClient, Prisma } from '@prisma/client';
 
 interface Iresult {
   cas: string;
+  data_name:string;
   category_name: string;
   category_path: string[];
 }
-
 const prisma = new PrismaClient();
 
 async function flows() {
@@ -17,6 +17,7 @@ async function flows() {
     take:88,
     select: {
       cas: true,
+      data_name:true,
       category_name: true,
       category_path: true,
     },
@@ -29,10 +30,12 @@ async function flows() {
   flows?.forEach(item => {
     // Defining json item
     let result: Iresult = {
+      data_name:'',
       cas: '',
       category_name: '',
       category_path: [''],
     };
+    result.data_name = item?.data_name;
     result.cas = item?.cas;
     result.category_name = item?.category_name;
     const category_path_list = item?.category_path as Prisma.JsonArray;
@@ -47,19 +50,43 @@ async function flows() {
   return resultJson;
 }
 
+
+interface Iresult1 {
+  data_name:string;
+  description:string;
+}
+async function flow_properties() {
+  // Data form database
+  const flow_properties = await prisma.flow_properties.findMany({
+    take:88,
+    select: {
+      data_name:true,
+      description:true,
+    },
+  });
+
+  // Defining the new json array schema
+  let resultJson: any[] = [];
+
+  // Fill in the data
+  flow_properties?.forEach(item => {
+    // Defining json item
+    let result: Iresult1 = {
+      data_name:'',
+      description: '',
+    };
+    result.data_name = item?.data_name;
+    result.description = item?.description;
+    resultJson.push(result);
+  });
+
+  return resultJson;
+}
+
 const resolvers = {
   Query: {
-    allFlows: () => {
-      return flows();
-    },
-    flowProperties: () => {
-      return prisma.flow_properties.findMany({
-        select: {
-          id: true,
-          data_name: true,
-        },
-      });
-    },
+    allFlows: () => {return flows();},
+    flowProperties: () => {return flow_properties();},
   },
 };
 
